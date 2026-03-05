f99304b7740003f8acfc2d9a3cdfb916d35c561b
bernard teye <bernardteye21@gmail.com>
2026-03-05 10:14:26 +0000

    Week 1 - Completed Student class and working program

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
-// main c++ file 
+#include <iostream> 
+#include <fstream> 
+#include <string> 
+#include <vector> 
+using namespace std; 
+ 
+class Student { 
+private: 
+    string indexNumber; 
+    string name; 
+public: 
+    Student() {} 
+    Student(string idx, string n) : indexNumber(idx), name(n) {} 
+    string getIndex() { return indexNumber; } 
+    string getName() { return name; } 
+    void display() { cout << indexNumber << " - " << name << endl; } 
+}; 
+ 
+int main() { 
+    cout << "Digital Attendance System Working!" << endl; 
+    return 0; 
+} 
